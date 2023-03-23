8b53d15f1244bb815e0ea5fea0f835b46b7db646
Aribunfi <arianna@pabis.de>
2023-03-23 20:15:56 +0100

    stampa a schermo dei film

--- index.php
+++ index.php
@@ -37,37 +37,41 @@ class Movie {
 $movies = [
     new Movie ("Fuga per la vittoria", "https://portaledelnazismo.altervista.org/wp-content/uploads/2015/10/1b17.jpg", "John Uston"),
      
-    new Movie("Matilda sei mitica", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxuGhQmY5OmEwHeZBplYNtlzhkT09O1x50Cg&usqp=CAU", "Danny De Vito")
+    new Movie ("Matilda sei mitica", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxuGhQmY5OmEwHeZBplYNtlzhkT09O1x50Cg&usqp=CAU", "Danny De Vito")
 ]
 
 
 ?>
 
-    <!DOCTYPE html>
-<html lang="it">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>MOVIES</title>
-</head>
-<body>
-  <div class="container">
-      <h1>Movies</h1>
-      <div class="row-cols-3">
-      <?php foreach($movies as $movie) : ?>
-          <div class="col">
-            <div class="card">
-              <img src="<? $movie->poster ?>" class="card-img-top" alt="">
-            <h2 class="card-title"><?= $movie->title ?></h2>
+<!DOCTYPE html>
+  <html lang="it">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MOVIES</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>
+
+  <body>
+    <div class="container">
+        <h1>Movies</h1>
+        <div class="row-cols-3">
+        <?php foreach($movies as $movie) : ?>
+            <div class="col">
+              <div class="card">
+                <img src="<?= $movie->poster ?>" class="card-img-top" alt="">
                 
-               <h3><? $movie->author ?></h3>
-                <hr>
-            <?php endforeach; ?>
+                <h2 class="card-title"><?= $movie->title ?></h2>
+                  
+                <h3><?= $movie->author ?></h3>
+                  <hr>
+              <?php endforeach; ?>
+            </div>
           </div>
         </div>
-      </div>
-  </div>
+    </div>
 
-</body>
+  </body>
+
 </html>
